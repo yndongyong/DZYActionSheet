ea8ed09edcab713ec5ff983bc30680841fd2da53
yndongyong <yndongong@foxmail.com>
2016-08-23 18:03:51 +0800

    提交一波

--- app/src/main/java/org/yndongyong/widget/actionsheet/demo/MainActivity.java
+++ app/src/main/java/org/yndongyong/widget/actionsheet/demo/MainActivity.java
@@ -1,8 +1,12 @@
 package org.yndongyong.widget.actionsheet.demo;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +15,10 @@ import org.yndongyong.widget.actionsheet.DZYActionSheet;
 public class MainActivity extends AppCompatActivity {
 
     TextView open_action_sheet;
+    Button btn_hidden, btn_show;
+
+    int lastTop;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +30,60 @@ public class MainActivity extends AppCompatActivity {
                 openActionSheet();
             }
         });
+        btn_hidden = (Button) findViewById(R.id.btn_hidden);
+        btn_hidden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hidden();
+            }
+        });
+        btn_show = (Button) findViewById(R.id.btn_show);
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               show();
+            }
+        });
     }
 
     private void openActionSheet() {
         DZYActionSheet actionSheet = new DZYActionSheet(this);
         String[] arrays = {"拍照", "从相册中选择"};
-        actionSheet.show(this.getWindow().getDecorView(),arrays, new DZYActionSheet.OnDZYActionSheetListener() {
+        actionSheet.show(this.getWindow().getDecorView(), arrays, new DZYActionSheet.OnDZYActionSheetListener() {
             @Override
             public void onClick(String text, int position) {
-                Toast.makeText(MainActivity.this, "text: "+text+" ,position:"+position, Toast
+                Toast.makeText(MainActivity.this, "text: " + text + " ,position:" + position, Toast
                         .LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void hidden() {
+        lastTop = open_action_sheet.getTop();
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(open_action_sheet, View.Y,
+                lastTop, 1200);
+        objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float animatedValue = (float) animation.getAnimatedValue();
+                if (BuildConfig.DEBUG) Log.d("MainActivity", "animatedValue:" + animatedValue);
+            }
+        });
+        objectAnimator.setDuration(2000);
+        objectAnimator.start();
+    }
+    
+    public void show() {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(open_action_sheet, View.Y, 
+                -open_action_sheet.getTop(), lastTop);
+        objectAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float animatedValue = (float) animation.getAnimatedValue();
+                if (BuildConfig.DEBUG) Log.d("MainActivity", "animatedValue:" + animatedValue);
+            }
+        });
+        objectAnimator.setDuration(2000);
+        objectAnimator.start();
     }
 }
